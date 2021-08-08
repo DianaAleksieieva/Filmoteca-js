854b456eaedebe5d24aba3d9f3317a330340635e
valeraron <77055501+valeraron@users.noreply.github.com>
2021-08-08 16:56:51 +0300

    создана пагинация

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import './sass/main.scss';
+import 'tui-pagination/dist/tui-pagination.min.css';
 import './js/cardTemplates';
 import { getMovies } from './js/getMovies';
 import { MODE } from './js/constants';
+import Pagination from 'tui-pagination';
 
-const inputElement = document.querySelector(".search-input");
-const searchButtonElement = document.querySelector(".search-button");
+const inputElement = document.querySelector('.search-input');
+const searchButtonElement = document.querySelector('.search-button');
+const paginationContainer = document.querySelector('#tui-pagination-container');
 
-let inputValue = "";
+let inputValue = '';
 let currentContent = [];
 
+const optionsPagination = {
+  itemsPerPage: 1,
+  visiblePages: 10,
+  page: 1,
+};
+const pagination = new Pagination(paginationContainer, optionsPagination);
+pagination.on('afterMove', event => {
+  const currentPage = event.page;
+  console.log(currentPage);
+});
 
 getMovies(MODE.popular, inputValue).then(data => {
   currentContent = data;
   console.log(currentContent);
+  pagination.reset(currentContent.length);
 });
-
 
 const searchMoviesEvent = () => {
   if (!inputValue) {
@@ -26,15 +39,13 @@ const searchMoviesEvent = () => {
       console.log(currentContent);
       return;
     }
-    console.log("No results found.");
-  })
-}
+    console.log('No results found.');
+  });
+};
 
 const inputEvent = () => {
-  inputValue = inputElement.value.trim().replace(" ", '%20');
-}
+  inputValue = inputElement.value.trim().replace(' ', '%20');
+};
 
-inputElement.addEventListener("input", inputEvent);
-searchButtonElement.addEventListener("click", searchMoviesEvent)
-
-
+inputElement.addEventListener('input', inputEvent);
+searchButtonElement.addEventListener('click', searchMoviesEvent);
