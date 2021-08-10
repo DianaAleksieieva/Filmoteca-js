2b492afeb58da3865e07b1070871869f5e38caff
valeraron <77055501+valeraron@users.noreply.github.com>
2021-08-10 19:18:07 +0300

    тестовая пагинация

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,15 +1,30 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
-import { getMovies } from './getMovies';
 import { paginationContainer } from './constants';
+import { BASE_URL } from './constants';
+import { API_KEY } from './constants';
+import { GET_BY_NAME } from './constants';
+import { GET_POPULAR } from './constants';
+import { MODE } from './constants';
 
 const optionsPagination = {
-  itemsPerPage: 1,
+  itemsPerPage: 0,
   visiblePages: 10,
-  page: 1,
 };
 export const pagination = new Pagination(paginationContainer, optionsPagination);
+
 pagination.on('afterMove', event => {
   const currentPage = event.page;
-  console.log(currentPage);
+  fetchFilms(currentPage).then(data => renderImages(data.images));
 });
+
+function fetchFilms(page) {
+  return fetch(`${BASE_URL}${GET_POPULAR}?${API_KEY}&page=${page}&per_page=20`)
+    .then(res => res.json())
+    .then(data => ({ films: data.hits, total: data.total_pages }));
+}
+
+function renderImages(films) {
+  console.log('RENDER');
+  console.log(films);
+}
