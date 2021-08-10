3ca61244a8b33d82ff3c506bccbe1ba9930b96a7
Alexandr Pryadchenko <56647239+eternalvision@users.noreply.github.com>
2021-08-10 18:20:19 +0300

    added test storage

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './sass/main.scss';
 import './js/cardTemplates';
 import './js/pagination';
 import './js/homepage';
-
+import './js/localstorage';
 import './js/modal';
--- src/js/onClickHomeLibraryBtn.js
+++ src/js/onClickHomeLibraryBtn.js
@@ -1,17 +1,21 @@
 import { SearchForm } from './constants';
 import { WatchedQueueContainer } from './constants';
 import { headerHome } from './constants';
+import { galleryContainer } from './constants';
 
 const onClickLibraryBtn = () => {
-  SearchForm.style.display = 'none';
-  WatchedQueueContainer.style.display = 'block';
-  headerHome.classList.replace('header-home', 'header-library');
-}
+    headerHome.classList.replace('header-home', 'header-library');
+    WatchedQueueContainer.style.display = 'block';
+    SearchForm.style.display = 'none';
+    //
+    galleryContainer.style.display = 'none';
+};
 const onClickHomeBtn = () => {
-  headerHome.classList.replace('header-library', 'header-home');
-  WatchedQueueContainer.style.display = 'none';
-  SearchForm.style.display = '';
-}
+    headerHome.classList.replace('header-library', 'header-home');
+    WatchedQueueContainer.style.display = 'none';
+    SearchForm.style.display = '';
+    //
+    galleryContainer.style.display = 'block';
+};
 
-export { onClickLibraryBtn, onClickHomeBtn};
-
+export { onClickLibraryBtn, onClickHomeBtn };
