832791b4f16e54408968a22ed0642f2024f129c4
johnny21-9 <heartless6m@gmail.com>
2021-08-10 15:26:33 +0300

    modal toggle

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,33 @@
 import { refsModal } from './constants';
-function openModal() {
-     
-    refsModal.openModal.addEventListener('click', toggleModal);
+let movieCards;
+
     refsModal.closeModalBtn.addEventListener('click', toggleModal);
 
     function toggleModal(event) {
         event.preventDefault();
+        console.log(event.currentTarget);
         refsModal.modal.classList.toggle('is-hidden');
     }
-}
-openModal()
+
+
+
+const target = refsModal.openModal;
+const config = {
+    attributes: false,
+    childList: true,
+    subtree: false
+};
+
+const callback = function(mutationsList, observer) {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            movieCards = document.querySelectorAll('.photo-card')
+            movieCards.forEach((item) => { item.addEventListener('click', toggleModal) })
+            // movieCard.addEventListener('click', toggleModal);
+            console.log(movieCards);
+        }
+    }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(target, config);
