e3386b70626f2c03b4c0b63eb194a939ddad06f3
valeraron <77055501+valeraron@users.noreply.github.com>
2021-08-11 19:30:28 +0300

    налаштована пагинация

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,8 +3,8 @@ export const API_KEY = 'api_key=02ffb45ff648e22333eeb14df43cb0e4';
 export const GET_POPULAR = 'movie/popular';
 export const GET_BY_NAME = 'search/movie/';
 export const MODE = {
-    search: 'search',
-    popular: 'popular',
+  search: 'search',
+  popular: 'popular',
 };
 export const inputElement = document.querySelector('.search-input');
 export const searchButtonElement = document.querySelector('.search-button');
@@ -18,16 +18,14 @@ export const headerHome = document.querySelector('header');
 export const notificationMessage = document.querySelector('#message');
 
 export const refsModal = {
-    openModal: document.querySelector('.gallery'),
-    closeModalBtn: document.querySelector('.modal-closeButton'),
-    modal: document.querySelector('.modal-backdrop'),
-    modalContainer: document.querySelector('.modal-container')
-  };
-
+  openModal: document.querySelector('.gallery'),
+  closeModalBtn: document.querySelector('.modal-closeButton'),
+  modal: document.querySelector('.modal-backdrop'),
+  modalContainer: document.querySelector('.modal-container'),
+};
 
 export const alreadyWatched = document.querySelector('#alreadyWatched');
 export const queueWatched = document.querySelector('#queueWatched');
-
 
 export const watched = document.querySelector('#watched-btn');
 export const queue = document.querySelector('#queue-btn');
--- src/js/getMovies.js
+++ src/js/getMovies.js
@@ -1,22 +1,23 @@
-import axios from "axios";
-import { BASE_URL } from "./constants";
+import axios from 'axios';
+import { BASE_URL } from './constants';
 import { API_KEY } from './constants';
 import { GET_BY_NAME } from './constants';
 import { GET_POPULAR } from './constants';
-import { MODE } from "./constants";
+import { MODE } from './constants';
 
-export const getMovies = async (mode, input) => {
+export const getMovies = async (mode, input, page) => {
   try {
     if (mode === MODE.search) {
-      if (input !== "") {
-         return await axios.get(`${BASE_URL}${GET_BY_NAME}?${API_KEY}&query=${input}`);
+      if (input !== '') {
+        return await axios.get(
+          `${BASE_URL}${GET_BY_NAME}?${API_KEY}&query=${input}&page=${page}&per_page=20`,
+        );
       }
     }
     if (mode === MODE.popular) {
-       return await axios.get(`${BASE_URL}${GET_POPULAR}?${API_KEY}`);
+      return await axios.get(`${BASE_URL}${GET_POPULAR}?${API_KEY}&page=${page}&per_page=20`);
     }
   } catch (error) {
     console.log(error.message);
   }
-}
-
+};
