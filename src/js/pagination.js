e4e7cead87dcd046a26537830faa07f2eca803ae
valeraron <77055501+valeraron@users.noreply.github.com>
2021-08-12 08:42:43 +0300

    настройка пагинации

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -3,6 +3,7 @@ import { paginationContainer } from './constants';
 const optionsPagination = {
   itemsPerPage: 1,
   visiblePages: 10,
+
   page: 1,
 };
 export const pagination = new Pagination(paginationContainer, optionsPagination);
