50c2d918cc9908cf3836d60a4f0717319d14b2f9
johnny21-9 <heartless6m@gmail.com>
2021-08-12 03:38:20 +0300

    modal close fix/genre fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,10 +6,17 @@ import modalTpl from '../templates/modalMovieCardTpl.hbs';
 
 let movieCards;
 let movieID;
-let movieContent
+let movieContent;
+
 refsModal.closeModalBtn.addEventListener('click', modalClose);
 
-
+refsModal.modal.addEventListener('click', (event) => {
+    if (event.target !== refsModal.modal) {
+        return
+    }
+    modalClose();
+});
+ 
 
 const target = refsModal.openModal;
 const config = {
@@ -35,20 +42,31 @@ observer.observe(target, config);
 function modalOpen(event) {
     event.preventDefault();
     movieID = event.currentTarget.dataset.id;
+
     getMovie().then(movieData => {
         movieContent = movieData;
+        movieContent.mainGenre = movieContent.genres[0].name;
         refsModal.modalContainer.insertAdjacentHTML('afterbegin', modalTpl(movieContent));
-        console.log(movieContent);
+        console.log(movieContent.mainGenre);
     });
+
     refsModal.modal.classList.toggle('is-hidden');
     document.querySelector('body').classList.add('scroll-blocked');
+    window.addEventListener('keydown', onEscKeydown);
 }
 
 
 function modalClose() {
+    window.removeEventListener('keydown', onEscKeydown);
     refsModal.modal.classList.toggle('is-hidden');
     refsModal.modalContainer.innerHTML = '';
     document.querySelector('body').classList.remove('scroll-blocked');
+}
+
+function onEscKeydown(event) {
+    if (event.code === 'Escape') {
+        modalClose()
+    }
 }
 
 
