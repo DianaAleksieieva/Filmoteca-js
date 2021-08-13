29f4c79fb7ef84787b63d7d7f79dabdff9f5d834
valeraron <77055501+valeraron@users.noreply.github.com>
2021-08-13 15:04:41 +0300

    подправил код

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,22 +1,34 @@
 import Pagination from 'tui-pagination';
 import { paginationContainer } from './constants';
-const optionsPagination1 = {
-  itemsPerPage: 1,
-  visiblePages: 9,
-  centerAlign: true,
-  page: 1,
-};
-const optionsPagination2 = {
+const optionsPagination = {
   itemsPerPage: 1,
   visiblePages: 5,
   centerAlign: true,
   page: 1,
+  emplate: {
+    page: '<a href="#" class="tui-page-btn">{{page}}p</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}p</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
 };
 const paginationAdaptive = () => {
   if (document.documentElement.clientWidth > 320) {
-    return optionsPagination1;
+    optionsPagination.visiblePages = 9;
+    return optionsPagination;
   } else {
-    return optionsPagination2;
+    console.log(optionsPagination.visiblePages);
+    return optionsPagination;
   }
 };
 export const pagination = new Pagination(paginationContainer, paginationAdaptive());
