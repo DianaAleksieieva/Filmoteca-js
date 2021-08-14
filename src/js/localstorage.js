8361173810eac85ce368f2313ad7fcb8e9ef8da0
Alexandr Pryadchenko <56647239+eternalvision@users.noreply.github.com>
2021-08-14 19:42:00 +0300

    local
    реализовано добавление элементов в массив, и отправка его в localstorage

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -3,20 +3,21 @@ import { queueWatched } from './constants';
 
 import { getMovie } from './modal';
 
+let watchedMass = [];
+let queueMass = [];
+
 alreadyWatched.onclick = function() {
-    localStorage.setItem(
-        'Watched',
-        getMovie().then(function(value) {
-            JSON.stringify(value);
-        }),
-    );
+    getMovie().then(function(value) {
+        localStorage.setItem('Watched', JSON.stringify(value));
+        watchedMass.push(value);
+        localStorage.setItem('Watched', JSON.stringify(watchedMass));
+    });
 };
 
 queueWatched.onclick = function() {
-    localStorage.setItem(
-        'Queue',
-        getMovie().then(function(value) {
-            JSON.stringify(value);
-        }),
-    );
+    getMovie().then(function(value) {
+        localStorage.setItem('Queue', JSON.stringify(value));
+        queueMass.push(value);
+        localStorage.setItem('Queue', JSON.stringify(queueMass));
+    });
 };
--- src/js/onClickHomeLibraryBtn.js
+++ src/js/onClickHomeLibraryBtn.js
@@ -3,19 +3,17 @@ import { WatchedQueueContainer } from './constants';
 import { headerHome } from './constants';
 import { galleryContainer } from './constants';
 
-
 const onClickLibraryBtn = () => {
     headerHome.classList.replace('header-home', 'header-library');
     WatchedQueueContainer.style.display = 'block';
     SearchForm.style.display = 'none';
     //
     galleryContainer.style.display = 'none';
-
 };
 const onClickHomeBtn = () => {
     headerHome.classList.replace('header-library', 'header-home');
     WatchedQueueContainer.style.display = 'none';
-    SearchForm.style.display = '';
+    SearchForm.style.display = 'block';
     //
     galleryContainer.style.display = 'block';
 };
