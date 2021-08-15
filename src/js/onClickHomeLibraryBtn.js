23e57e528d0aef11a91cef41b5f04f20a99b3cf0
johnny21-9 <heartless6m@gmail.com>
2021-08-15 05:12:54 +0300

    localStorageDeletion

--- src/js/onClickHomeLibraryBtn.js
+++ src/js/onClickHomeLibraryBtn.js
@@ -2,13 +2,16 @@ import { SearchForm } from './constants';
 import { WatchedQueueContainer } from './constants';
 import { headerHome } from './constants';
 import { galleryContainer } from './constants';
+import { clearGalleryMarkup } from './utils';
 
 const onClickLibraryBtn = () => {
     headerHome.classList.replace('header-home', 'header-library');
     WatchedQueueContainer.style.display = 'block';
     SearchForm.style.display = 'none';
-    //
-    galleryContainer.style.display = 'none';
+    // galleryContainer.style.display = 'none';
+    clearGalleryMarkup();
+
+
 };
 const onClickHomeBtn = () => {
     headerHome.classList.replace('header-library', 'header-home');
@@ -16,6 +19,8 @@ const onClickHomeBtn = () => {
     SearchForm.style.display = 'block';
     //
     galleryContainer.style.display = 'block';
+
+    localStorage.setItem('CurrentGalleryPage', 'Home');
 };
 
 export { onClickLibraryBtn, onClickHomeBtn };
