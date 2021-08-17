0e4309038aed0ee5bb027653c22e71ab2c3de9f5
Diana Aleksieieva <aleksodiana@gmail.com>
2021-08-17 21:21:23 +0300

    Update onClickHomeLibraryBtn.js

--- src/js/onClickHomeLibraryBtn.js
+++ src/js/onClickHomeLibraryBtn.js
@@ -10,10 +10,8 @@ const onClickLibraryBtn = () => {
     headerEl.SearchForm.style.display = 'none';
     headerEl.headerNavigationItemHome.classList.remove('navigation__item--current');
     headerEl.headerNavigationItemLibrary.classList.add('navigation__item--current');
-    showSpinner();
     clearSpiner();
     hidePagination();
-    // galleryContainer.style.display = 'none';
     clearGalleryMarkup();
 };
 const onClickHomeBtn = () => {
@@ -22,7 +20,6 @@ const onClickHomeBtn = () => {
     headerEl.headerHome.classList.replace('header-library', 'header-home');
     mainEl.WatchedQueueContainer.style.display = 'none';
     headerEl.SearchForm.style.display = 'inline-block';
-    showSpinner();
     clearSpiner();
     showPagination();
     mainEl.galleryContainer.style.display = 'flex';
