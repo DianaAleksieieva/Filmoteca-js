59572d8a9ad9363a944913f1a1eda88d6d392b4c
Diana Aleksieieva <aleksodiana@gmail.com>
2021-08-19 01:00:21 +0300

    add-css-watched-queue

--- src/js/onClickHomeLibraryBtn.js
+++ src/js/onClickHomeLibraryBtn.js
@@ -4,12 +4,12 @@ import { clearSpiner, showSpinner } from './spinner';
 import { hidePagination, showPagination } from './pagination';
 
 const onClickLibraryBtn = () => {
-    console.log('qqqqqq');
     headerEl.headerHome.classList.replace('header-home', 'header-library');
     mainEl.WatchedQueueContainer.style.display = 'block';
     headerEl.SearchForm.style.display = 'none';
     headerEl.headerNavigationItemHome.classList.remove('navigation__item--current');
     headerEl.headerNavigationItemLibrary.classList.add('navigation__item--current');
+    headerEl.watchedButton.classList.add('watched-queue__button--active');
     clearSpiner();
     hidePagination();
     clearGalleryMarkup();
@@ -22,7 +22,6 @@ const onClickHomeBtn = () => {
     headerEl.SearchForm.style.display = 'inline-block';
     clearSpiner();
     showPagination();
-
     localStorage.setItem('CurrentGalleryPage', 'Home');
 };
 
